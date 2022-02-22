ba5c994e0c10bcf6a3cd21165738048ec845536f
Hopeless2 <89778966354@mail.ru>
2022-02-22 20:49:09 +0300

    Commit init

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,40 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+// Stub
+public class PostRepository {
+  private final ConcurrentSkipListMap<Long, Post> posts = new ConcurrentSkipListMap<>();
+  private long newId = 1L;
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.of(posts.get(id));
+  }
+
+  public Post save(Post post) {
+    if(post.getId() == 0){
+      post.setId(newId);
+      posts.putIfAbsent(newId, post);
+      newId++;
+    }else if(post.getId() < newId && post.getId() > 0){
+      posts.replace(post.getId(), post);
+    }else{
+      throw new NotFoundException("Пост не найден");
+    }
+    return post;
+  }
+
+  public Optional<Post> removeById(long id) {
+    return Optional.of(posts.remove(id));
+  }
+}
